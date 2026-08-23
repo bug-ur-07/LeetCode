6fe527ddd6fdf7c49ec18dac0d18bd3305e62a48
AMAN KUMAR <anudhn2009@gmail.com>
2026-08-23 15:38:44 +0530

    Time: 6 ms (97.08%), Space: 46.4 MB (99.27%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,58 @@
+class Solution {
+    public boolean sumGame(String num) {
+        char ch[]  = num.toCharArray();
+
+        int ls = 0;
+        int rs = 0;
+
+        int lb = 0;
+        int rb = 0;
+        int s = 0;
+        int e = ch.length-1;
+
+        while(s<e){
+
+      // Left side
+            if (ch[s] == '?') {
+                lb++;
+            } else {
+                ls += ch[s] - '0';
+            }
+
+            // Right side
+            if (ch[e] == '?') {
+                rb++;
+            } else {
+                rs += ch[e] - '0';
+            }
+            s++;
+            e--;
+        }
+
+        if(lb+rb==0){            
+            return ls-rs != 0 ;
+        }
+        if(rs+ls ==0){
+            if(lb==rb)return false;
+            return true;
+        }
+        if((lb+rb)%2 !=0)return true;
+        int min = Math.min(lb , rb);
+        lb -=min;
+        rb -=min;
+        if(lb>rb){
+            if(ls>rs){
+                return true;
+            }else {
+                if(9* (lb/2) == rs-ls) return false;
+            }
+        }else {
+            if(rs>ls) return true;
+            else {
+                if(9 * (rb/2) == ls-rs) return false;
+                else return true;
+            }
+        }
+            return true;
+    }
+}
